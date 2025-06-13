70f9f736452dd8e29ca8b0fccc0f9f8b985051bb
Lofi-GiRL237 <juniortchoc81@gmail.com>
2025-06-13 14:24:17 +0100

    Add files via upload

---
+++ Hotel reservation system/introduction.php
@@ -0,0 +1,21 @@
+<?php include 'header.php';?>
+<div class="container">
+
+       <h1 class="title">Introduction</h1>
+       <div class="row">
+              <div class="col-sm-4"><p></p>                
+          </p></div>
+              <div class="col-sm-4"><p>At Hotel Sky, we don’t want to simply offer a comfortable and affordable place to stay. Our vision is to ensure that every guest and visitor has an unforgettable and extraordinary experience. From our seamless app-based check-in and room access system, to our beautiful decor, we have spared no expense in making our hotels as unique and beautiful as possible. But we haven’t only invested in mind-blowing technology and beautiful design, more importantly, we have invested in great people. While facilities, technology, amenities and indulgences are important, what really gives Hotel Sky its heart is our incredible team of dedicated and passionate professionals. Individuals who live and breathe the Hotel Sky dream. Your dream. Our reality.</p></div>
+                           
+       </div>
+
+
+       <div class="spacer">
+       <div class="embed-responsive embed-responsive-16by9"><iframe  class="embed-responsive-item" src="//player.vimeo.com/video/55057393?title=0" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+       </div>
+
+
+
+
+</div>
+<?php include 'footer.php';?>
